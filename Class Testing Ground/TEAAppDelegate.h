969ce9b68234930909e3a3e3c4c38cbd9f0aca96
Tony Angelo <tonyangelox@veronica.home>
2014-06-08 19:38:03 -0400

    Initial Commit

---
+++ Class Testing Ground/TEAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEAAppDelegate.h
+//  Class Testing Ground
+//
+//  Created by Tony Angelo on 6/8/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Class Testing Ground/TEAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEAViewController.h
+//  Class Testing Ground
+//
+//  Created by Tony Angelo on 6/8/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAViewController : UIViewController
+
+@end
